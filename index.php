3e5af53472fb3482d84cb48d7f6feb592cbb37c9
Kacperendo <92695386+Besp1N@users.noreply.github.com>
2023-07-22 23:48:19 +0200

    added html

--- index.php
+++ index.php
@@ -5,3 +5,46 @@ namespace App;
 
 require_once("src/Utils/debug.php");
 
+if(!empty($_GET['action'])){
+    $action = $_GET['action'];
+}
+else{
+    $action = null;
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="header">
+        <h1>Moje notatki</h1>
+    </div>
+    <div class="container">
+        <div class="menu">
+            <ul>
+                <li><a href="?action=sth">Lista notatek</a></li>
+                <li><a href="?action=create">Nowa notatka</a></li>
+            </ul>
+        </div>
+        <div>
+            <?php
+                if($action === "create"):
+                    echo "nowa notatka";
+                else:
+                    echo "lista notatek";
+                endif;
+            ?>
+        </div>
+    </div>
+    <div class="footer">
+
+    </div>
+</body>
+</html>
