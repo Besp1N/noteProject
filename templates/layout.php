2ec45e371450096ef1da5c6f8f56ee48c333c0bd
Kacperendo <92695386+Besp1N@users.noreply.github.com>
2023-07-23 22:43:29 +0200

    fixed css error

--- templates/layout.php
+++ templates/layout.php
@@ -4,7 +4,7 @@
   <title>Notatnik</title>
   <meta charset="utf-8">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css">
-  <link href="/public/style.css" rel="stylesheet">
+  <link rel="stylesheet" href="public/style.css">
 </head>
 
 <body class="body">
