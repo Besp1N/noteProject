d40195090d67027d4116ebd4ddf82011b8884c83
Kacperendo <92695386+Besp1N@users.noreply.github.com>
2023-07-24 21:15:02 +0200

    added run methon in controller

--- index.php
+++ index.php
@@ -5,43 +5,12 @@ declare(strict_types=1);
 namespace App;
 
 require_once("src/Utils/debug.php");
-require_once("src/View.php");
+require_once("src/Controller.php");
 
 const DEFAULT_ACTION = 'list';
 
 $action = $_GET['action'] ?? DEFAULT_ACTION;
 
 $controller = new Controller();
-$view = new View();
+$controller->run($action);
 
-$viewParams = [];
-
-switch($action){
-  case "create":
-    $page = 'create';
-    $created = false;
-    if(!empty($_POST)){
-      $created = true;
-      $viewParams = [
-        "title" => $_POST["title"],
-        "description" => $_POST["description"]
-      ];
-    }
-    $viewParams["created"] = $created;
-    break;
-
-  case "show":
-    $viewParams = [
-      "title" => "Moja notatka",
-      "description" => "Opis"
-    ];
-    break;
-
-  default:
-    $page = 'list';
-    $viewParams['resultList'] = "wyświetlamy notatki";
-  break;
-}
-
-
-$view->render($page, $viewParams);
