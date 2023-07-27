8e4f621216af382dd21d64d2dc3392452cdaa3d5
Kacperendo <92695386+Besp1N@users.noreply.github.com>
2023-07-27 20:51:05 +0200

    sql connecton

--- index.php
+++ index.php
@@ -7,12 +7,17 @@ namespace App;
 require_once("src/Utils/debug.php");
 require_once("src/Controller.php");
 
+$configuration = require_once("config/config.php");
+dump($configuration);
+
+
 $request = [
   'get' => $_GET,
   'post' => $_POST
 ];
 
 
-$controller = new Controller($request);
-$controller->run();
+// $controller = new Controller($request);
+// $controller->run();
 
+(new Controller($request))->run();
